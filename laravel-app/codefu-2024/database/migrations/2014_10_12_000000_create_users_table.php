24244f0cfbff23c03f3eeeb016ec218fe2ceb146
geeloov <vladimirgelov0@gmail.com>
2024-11-26 14:07:21 +0100

    fix in migrations

--- laravel-app/codefu-2024/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel-app/codefu-2024/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,6 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('username')->unique()->nullable();
-            $table->string('')
             $table->string('email')->unique();
             $table->string('google_id')->nullable();
             $table->string('facebook_id')->nullable();
